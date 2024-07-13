97930a6cb44224ad657a6e93c67b3463eb85531e
TheNods <jn.opperman@gmail.com>
2024-07-13 16:36:37 +0200

    Collectstatic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+python3 manage.py collectstatic
 # celery --app=config worker -l INFO -Q celery,celery:1,celery:2,celery:3
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 8 --log-level info
 
